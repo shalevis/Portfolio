0fb1d3e4341689c4f0e49a6faee5ba2adcdeb4da
shalevis <shalevi55344@gmail.com>
2025-11-17 20:26:44 +0200

    first attempt

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-
-  preview: {
-    host: true,
-    port: 80,
-    strictPort: true,
-    allowedHosts: ["*"],
-  },
-
-  server: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ["*"],
-  }
+  base: "/portfolio/",         
+  plugins: [react()]
 });
