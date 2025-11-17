305a4ebdc72f9842c45e89c101725e0e6f34c1d3
shalevis <shalevi55344@gmail.com>
2025-11-17 19:59:14 +0200

    first attempt

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   preview: {
     host: true,
     port: 80,
-    allowedHosts: ["crimson-wolf-15259.zap.cloud"]
+    strictPort: true,
+    allowedHosts: ["*"],
   },
+
   server: {
-    host: true
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["*"],
   }
 });
