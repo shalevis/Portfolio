a82d325e173f07d6dac140267d7cf811cddd3ba2
shalevis <shalevi55344@gmail.com>
2025-11-17 21:12:39 +0200

    first attempt

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import About from './components/About'
 import Experience from './components/Experience'
 import Education from './components/Education'
 import Contact from './components/Contact' 
-import Pipeline from './components/Pipeline'
 
 
 export default function App(){
